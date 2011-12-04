ec0ec8d3e4d9a77a02820b7bfd54b496f5f896bf
Thomas Waldecker <thomas.waldecker@googlemail.com>
2011-12-04 16:16:51 +0100

    added template

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Trailsy::Application.routes.draw do
+  
+  root :to => "home#index"
+  
+  #get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
