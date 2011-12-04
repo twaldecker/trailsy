7ad3a13fb3ac7a9e11b7ce27cf15838c2becd21f
Thomas Waldecker <thomas.waldecker@googlemail.com>
2011-12-04 15:50:05 +0100

    added rails project

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Transi #
+
+Transi is a translation web-application. Its focus is on the user interface (UI) and a easy way storing and retreving the translation data.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trailsy::Application.config.secret_token = '82cee865b9c805d248d64c515706f427460ddbd1673a4d5fe16f072c15491608841b185298008928d6f056f56ee90b1ef85d5581f84ea27d8de67b993916b0c0'
