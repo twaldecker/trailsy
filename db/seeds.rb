92b1b55b6ba750e19bd3c0a9b84f182a8f8575ae
Aco Mitevski <acomitevski@googlemail.com>
2011-12-15 22:30:00 +0100

    added some seeds/fixtures for testing purpose

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+langs   = Language.create([{:code => 'en', :language => 'English'},
+                           {:code => 'de', :language => 'Deutsch'},
+                           {:code => 'mk', :language => 'Makedonski'}])
+
+screen = Word.create({   :word        => 'Screen',
+                         :example     => 'The screen is nice',
+                         :language_id => 1})
+screen.translations.create([
+                      {  :word        => 'Monitor',
+                         :example     => 'Der Monitor hat eine gute Auflösung',
+                         :language_id => 2},
+                       { :word        => 'Ekran',
+                         :example     => 'Ekranot e mnogu dobar',
+                         :language_id => 3},
+                       { :word        => 'Bildschirm',
+                         :example     => 'Der Bildschirm hat eine gute Auflösung',
+                         :language_id => 2}
+                      ])
