b792f3a775a38e49aafdaaecd8ab3718f02dd2c2
Thomas Waldecker <thomas.waldecker@googlemail.com>
2011-12-18 18:16:09 +0100

    added voteable and voter behaviours

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,4 +1,6 @@
 class Connection < ActiveRecord::Base
   belongs_to :lang1, :class_name => "Word"
   belongs_to :lang2, :class_name => "Word"
+  
+  acts_as_voteable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
   validates_presence_of :email
   validates_uniqueness_of :email
   
+  acts_as_voter
+  
   def self.authenticate(email, password)
     user = find_by_email(email)
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
