62e504f8cd2d158d6cd3c94f4da0b87077b431eb
Thomas Waldecker <thomas.waldecker@googlemail.com>
2011-12-28 15:03:12 +0100

    added verification

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Trailsy::Application.routes.draw do
   get "log_out" =>"sessions#destroy", :as => "log_out"  
   get "log_in" =>"sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
+
+  match 'user/:user/validate/:code' => 'users#validate_email'
+
   resources :users
   resources :sessions
   
