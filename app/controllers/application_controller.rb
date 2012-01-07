c43072adc30c544fc5421d30c928fd23a0488ac5
Aco Mitevski <acomitevski@googlemail.com>
2012-01-07 23:55:07 +0100

    added internationalisation for rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,20 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
   helper_method :filter_params
+  before_filter :set_locale
+
+  def set_locale
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    I18n.locale = extract_locale_from_accept_language_header
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
+
 
   private
 
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
   def logged_in?
     if session[:user_id]
       true
