5fcc31d5713e088366e44679fd4c9c060d9cbe78
Aco Mitevski <acomitevski@googlemail.com>
2012-01-08 11:01:24 +0100

    added jsonCache and  new build

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,21 @@
 require.config({
     paths: {
         'underscore': 'vendor/underscore',
-        'backbone': 'vendor/backbone'
+        'backbone': 'vendor/backbone',
+        'json2': 'vendor/json2',
+        'jsoncache': 'vendor/jsoncache'
     }
 })
 
 require(['jquery',
          'underscore',
-         'backbone','app','domReady', 'vendor/detectmobilebrowser'],
-    function($,_,Backbone,app, domReady, detection){
+         'backbone',
+         'app',
+         'domReady',
+         'vendor/detectmobilebrowser',
+         'jsoncache',
+         'json2'],
+    function($,_,Backbone,app, domReady, detection, jsoncache, json2){
         domReady(function() {
             app.init();
         });
