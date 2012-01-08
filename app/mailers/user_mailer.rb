3326399f885c52604d5416d0808ee2bb9fee8b61
Thomas Waldecker <thomas.waldecker@googlemail.com>
2012-01-08 21:34:22 +0100

    translated mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,14 @@ class UserMailer < ActionMailer::Base
     @host = host
     mail(:to => @user.email, :subject => 'Email verification')
   end
+  
+  private
+  
+  #see: http://blog.andischacke.com/2009/10/localized-actionmailer-templates-for.html
+  # we override the template_path to render localized templates (since rails does not support that :-( )
+  # This thing is not testable since you cannot access the instance of a mailer...
+  def initialize_defaults(method_name)
+    super
+    @template = "#{I18n.locale}_#{method_name}"
+  end
 end
