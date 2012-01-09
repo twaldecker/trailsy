4d7bce41fe9b36e53a3bd073bdd948c57593f008
Aco Mitevski <acomitevski@googlemail.com>
2012-01-09 18:48:31 +0100

    compress response

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Trailsy::Application.configure do
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
-
+  config.static_cache_control = "public, max-age=3600"
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
